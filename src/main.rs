0a18323142046170a458272653313ec9315d757c
Nolan Rosen <nolanrosen15@gmail.com>
2019-11-06 19:57:53 -0500

    First compilation of amethyst

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
 #![warn(clippy::all)]
-fn main() {
-    println!("Hello, world!");
+use amethyst::prelude::*;
+use amethyst::winit::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+
+struct GameState;
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, _: StateData<'_, GameData<'_, '_>>) {
+        println!("Starting game!");
+    }
+
+    fn handle_event(&mut self, _: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
+        if let StateEvent::Window(event) = &event {
+            match event {
+                 Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::KeyboardInput {
+                        input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Escape), .. }, ..
+                    } |
+                    WindowEvent::CloseRequested => Trans::Quit,
+                    _ => Trans::None,
+                },
+                _ => Trans::None,
+            }
+        } else {
+            Trans::None
+        }
+    }
+
+    fn update(&mut self, _: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        println!("Computing some more whoop-ass...");
+        Trans::Quit
+    }
+}
+
+fn main() -> amethyst::Result<()> {
+    let assets_dir = "assets/";
+    let mut game = Application::new(assets_dir, GameState, GameDataBuilder::default())?;
+    amethyst::start_logger();
+    game.run();
+    Ok(())
 }
