732ef4bffd1dea70d5a5bde35555146234fcfcdf
Nolan Rosen <nolanrosen15@gmail.com>
2019-11-24 07:13:37 -0500

    WIP game saves

--- src/saves/mod.rs
+++ src/saves/mod.rs
@@ -2,6 +2,13 @@
 //! This includes, but not limited to, the tile's metadata, the pawns metadata, and map's
 //! metadata.
 
+pub struct IntermediatePawn {
+    position: [u8; 3],
+    type: String,
+    name: String,
+    skills: Vec<Skill>
+}
+
 /// This is the structured form of the file the game loads and saves to.
 pub struct GameSave {
     tiles: Vec<Tile>,
